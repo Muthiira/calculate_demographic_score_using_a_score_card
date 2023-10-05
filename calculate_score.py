0baa2faecd76fd3069e52ed5c8e3083b61b39604
Muthiira <calebmuchiri48@gmail.com>
2023-10-05 17:13:15 +0300

    demographic_score

--- calculate_score.py
+++ calculate_score.py
@@ -1,438 +1,59 @@
 import json
 
-scorecard_json = {
-    "id": 30536,
-    "version_id": 0,
-    "decision_model_id": 4169,
-    "decision_type": "scoring",
-    "setting": {
-        "age": {
-            "type": "rules",
-            "weight": "7",
-            "attributes": [
-                {
-                    "rule": [
-                        { "condition": "value < 20", "score": 0 },
-                        { "condition": "value < 30", "score": 6 },
-                        { "condition": "value < 40", "score": 10 },
-                        { "condition": "value < 50", "score": 8 },
-                        { "condition": "value < 60", "score": 4 },
-                        { "condition": True, "score": 0 }
-                    ]
-                }
-            ],
-            "description": "Age",
-            "maximum_score": "10"
-        },
-        "gender": {
-            "type": "string",
-            "weight": "10",
-            "attributes": [
-                {
-                    "key": "female",
-                    "score": 10
-                },
-                {
-                    "key": "male",
-                    "score": 5
-                }
-            ],
-            "description": "Gender",
-            "maximum_score": "10"
-        },
-        "location": {
-            "type": "string",
-            "weight": "5",
-            "attributes": [
-                {
-                    "key": "City",
-                    "score": 5
-                },
-                {
-                    "key": "Town",
-                    "score": 3
-                },
-                {
-                    "key": "Village",
-                    "score": 2
-                }
-            ],
-            "description": "Location",
-            "maximum_score": "10"
-        },
-        "customer_tier": {
-            "type": "string",
-            "weight": "5",
-            "attributes": [
-                {
-                    "key": "Tier 3",
-                    "score": "10"
-                },
-                {
-                    "key": "Tier 2",
-                    "score": "6"
-                },
-                {
-                    "key": "Tier 1",
-                    "score": "2"
-                }
-            ],
-            "description": "Customer Tier",
-            "maximum_score": "10"
-        },
-        "marital_status": {
-            "type": "string",
-            "weight": "5",
-            "attributes": [
-                {
-                    "key": "Married",
-                    "score": "10"
-                },
-                {
-                    "key": "Single",
-                    "score": "6"
-                },
-                {
-                    "key": "Separated",
-                    "score": "4"
-                },
-                {
-                    "key": "Widowed",
-                    "score": "7"
-                },
-                {
-                    "key": "Divorced",
-                    "score": "4"
-                }
-            ],
-            "description": "Marital Status",
-            "maximum_score": "10"
-        },
-        "employer_category": {
-            "type": "string",
-            "weight": "5",
-            "attributes": [
-                {
-                    "key": "National Govt: Public",
-                    "score": "10"
-                },
-                {
-                    "key": "Local Govt: Public",
-                    "score": "5"
-                },
-                {
-                    "key": "Private Company",
-                    "score": "8"
-                },
-                {
-                    "key": "Enterprise",
-                    "score": "7"
-                },
-                {
-                    "key": "Non-profit",
-                    "score": "6"
-                }
-            ],
-            "description": "Employer Category",
-            "maximum_score": "10"
-        },
-        "employment_status": {
-            "type": "string",
-            "weight": "10",
-            "attributes": [
-                {
-                    "key": "Employed",
-                    "score": "10"
-                },
-                {
-                    "key": "Self-employed",
-                    "score": "7"
-                },
-                {
-                    "key": "Unemployed",
-                    "score": "0"
-                }
-            ],
-            "description": "Employment Status",
-            "maximum_score": "10"
-        },
-        "type_of_residence": {
-            "type": "string",
-            "weight": "5",
-            "attributes": [
-                {
-                    "key": "Own House",
-                    "score": "10"
-                },
-                {
-                    "key": "Rented Apartment",
-                    "score": "7"
-                },
-                {
-                    "key": "Parents Apartment",
-                    "score": "5"
-                }
-            ],
-            "description": "Type of Residence",
-            "maximum_score": "10"
-        },
-        "monthly_net_income": {
-            "type": "string",
-            "weight": "10",
-            "attributes": [
-                {
-                    "key": "Above 1,000,000",
-                    "score": "10"
-                },
-                {
-                    "key": "700,000 - 999,999",
-                    "score": "9"
-                },
-                {
-                    "key": "400,000 - 699,999",
-                    "score": "8"
-                },
-                {
-                    "key": "200,000 - 399,999",
-                    "score": "7"
-                },
-                {
-                    "key": "100,000 - 199,999",
-                    "score": "5"
-                },
-                {
-                    "key": "55,000 - 99,999",
-                    "score": "3"
-                },
-                {
-                    "key": "10,000 - 54,999",
-                    "score": "2"
-                }
-            ],
-            "description": "Monthly Net Income",
-            "maximum_score": "10"
-        },
-        "no_of_dependent": {
-            "type": "string",
-            "weight": "8",
-            "attributes": [
-                {
-                    "key": "0",
-                    "score": "5"
-                },
-                {
-                    "key": "1",
-                    "score": "8"
-                },
-                {
-                    "key": "2",
-                    "score": "10"
-                },
-                {
-                    "key": "3 or more",
-                    "score": "5"
-                }
-            ],
-            "description": "Number of Dependents",
-            "maximum_score": "10"
-        },
-        "sector_of_employment": {
-            "type": "string",
-            "weight": "5",
-            "attributes": [
-                {
-                    "key": "Agriculture",
-                    "score": "6"
-                },
-                {
-                    "key": "Banking",
-                    "score": "7"
-                },
-                {
-                    "key": "Education",
-                    "score": "5"
-                },
-                {
-                    "key": "Healthcare",
-                    "score": "6"
-                },
-                {
-                    "key": "Hospitality and Events",
-                    "score": "0"
-                },
-                {
-                    "key": "Information Technology",
-                    "score": "5"
-                },
-                {
-                    "key": "Law",
-                    "score": "10"
-                },
-                {
-                    "key": "Manufacturing and Construction",
-                    "score": "4"
-                },
-                {
-                    "key": "Media & Entertainment",
-                    "score": "7"
-                },
-                {
-                    "key": "NGO",
-                    "score": "0"
-                },
-                {
-                    "key": "Oil and Gas",
-                    "score": "6"
-                },
-                {
-                    "key": "Other Financial",
-                    "score": "6"
-                },
-                {
-                    "key": "Others",
-                    "score": "4"
-                },
-                {
-                    "key": "Public services and administration",
-                    "score": "5"
-                },
-                {
-                    "key": "Telecoms",
-                    "score": "9"
-                },
-                {
-                    "key": "Tourism & Hospitality",
-                    "score": "3"
-                },
-                {
-                    "key": "Transportation & Logistics",
-                    "score": "0"
-                },
-                {
-                    "key": "Wholesale and Retail Trade",
-                    "score": "6"
-                }
-            ],
-            "description": "Sector of Employment",
-            "maximum_score": "10"
-        },
-        "educational_attainment": {
-            "type": "string",
-            "weight": "5",
-            "attributes": [
-                {
-                    "key": "Msc and above",
-                    "score": "10"
-                },
-                {
-                    "key": "Bsc,HND and other equivalent",
-                    "score": "8"
-                },
-                {
-                    "key": "Diploma",
-                    "score": "5"
-                },
-                {
-                    "key": "School cert",
-                    "score": "5"
-                },
-                {
-                    "key": "Vocation/Technical",
-                    "score": "4"
-                },
-                {
-                    "key": "Apprenticeship and Crafts",
-                    "score": "0"
-                }
-            ],
-            "description": "Educational Attainment",
-            "maximum_score": "10"
-        },
-        "time_with_current_employer": {
-            "type": "rules",
-            "weight": "5",
-            "attributes": [
-                {
-                    "rule": [
-						{ "condition": "time_with_current_employer == 0", "score": 0 },
-						{ "condition": "time_with_current_employer <= 2", "score": 6 },
-						{ "condition": "time_with_current_employer <= 5", "score": 8 },
-						{ "condition": "time_with_current_employer <= 6", "score": 10 },
-						{ "condition": "time_with_current_employer <= 10", "score": 10 },
-						{ "condition": "time_with_current_employer < 100", "score": 10 },
-						{ "condition": True, "score": 0 }
-					]
-                }
-            ],
-            "description": "Time with Current Employer",
-            "maximum_score": "10"
-        }
-    }
-}
+# Import scorecard data from scorecard.json
+with open('scorecard.json', 'r') as scorecard_file:
+    scorecard = json.load(scorecard_file)
 
-# Parse the scorecard JSON into a Python dictionary
-scorecard = json.loads(json.dumps(scorecard_json))
+# Import borrower data from borrower_data.json
+with open('borrower_data.json', 'r') as borrower_data_file:
+    borrower_data = json.load(borrower_data_file)
+
 
 def calculate_demographic_score(borrower_data, scorecard):
+    # Initialize the total score to 0
     total_score = 0
 
+    # Iterate through each attribute in the scorecard settings
     for attribute, settings in scorecard['setting'].items():
+        # Extract attribute details from the scorecard settings
         attribute_type = settings['type']
         attribute_weight = int(settings['weight'])
         attribute_attributes = settings['attributes']
 
-        # Retrieve the value of the attribute from borrower_data
+        # Get the value of the attribute from borrower_data or None if not present
         attribute_value = borrower_data.get(attribute, None)
 
         if attribute_value is not None:
             if attribute_type == 'rules':
-                # Iterate through the rules and apply them
+                # Initialize the rule score to 0
+                rule_score = 0
+
+                # Iterate through the rules for this attribute
                 for rule in attribute_attributes[0]['rule']:
                     condition = rule['condition']
-                    if isinstance(condition, bool):
-                        if condition:
-                            rule_score = int(rule['score'])
-                            total_score += rule_score
-                            break  # Exit loop on the first matching rule
-                    else:
-                        condition_operator, condition_value = condition.split(' ', 1)
-                        if condition_operator == "<":
-                            if attribute_value < eval(condition_value):
-                                rule_score = int(rule['score'])
-                                total_score += rule_score
-                                break  # Exit loop on the first matching rule
-                        elif condition_operator == "<=":
-                            if attribute_value <= eval(condition_value):
-                                rule_score = int(rule['score'])
-                                total_score += rule_score
-                                break  # Exit loop on the first matching rule
+
+                    # Replace "value" in the condition with the actual attribute value
+                    condition = condition.replace("value", "attribute_value")
+
+                    # Evaluate the condition and update the rule score if it's True
+                    if eval(condition, {'attribute_value': attribute_value}):
+                        rule_score = int(rule['score'])
+                        break
+
+                # Add the minimum of rule_score and maximum_score to the total score
+                total_score += min(rule_score, int(settings['maximum_score']))
+
             elif attribute_type == 'string':
-                # Match the attribute value to predefined keys and assign the score
+                # Iterate through attribute attributes (key-value pairs)
                 for attr in attribute_attributes:
                     if attr['key'] == attribute_value:
+                        # Add the score associated with the attribute value to the total score
                         total_score += int(attr['score'])
-                        break  # Exit loop on the first matching key
+                        break
 
+    # Return the calculated total score
     return total_score
 
-
-borrower_data = {
-    "age": 40,
-    "gender": "female",
-    "location": "City",
-    "customer_tier": "Tier 2",
-    "marital_status": "Married",
-    "employer_category": "Private Company",
-    "employment_status": "Employed",
-    "type_of_residence": "Rented Apartment",
-    "monthly_net_income": "100,000 - 199,999",
-    "no_of_dependent": "2",
-    "sector_of_employment": "Information Technology",
-    "educational_attainment": "Bsc,HND and other equivalent",
-    "time_with_current_employer": 10
-}
-
-# Calculate the demographic score
 demographic_score = calculate_demographic_score(borrower_data, scorecard)
 
 print(f"Demographic Score: {demographic_score}")
